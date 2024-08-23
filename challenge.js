1d8bba5b993f36990b33ff44bd53dbcf59145e79
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-08-23 17:25:50 +0300

    my answers

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let a=100
+let b=99
+let c=95
+if(a>b || a>c){
+    console.log("The largest number out of the three numbers is a")
+}else{
+    console.log(" a is not largest number out of three numbers")
+}
